dbc01ca77bae4255254dc52b50f3c66a40393824
Hirofumi Shima <shima@peace2.jp>
2013-07-28 15:12:55 +0900

    Pureの導入

---
+++ SP.Labo.Web/Controllers/R2CSSController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SP.Labo.Web.Controllers
+{
+    public class R2CSSController : Controller
+    {
+        //
+        // GET: /R2CSS/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AllList()
+        {
+            return View();
+        }
+
+        public ActionResult Layout()
+        {
+            return View();
+        }
+
+    }
+}
