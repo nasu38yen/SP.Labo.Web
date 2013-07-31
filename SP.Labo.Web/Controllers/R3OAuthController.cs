07502dece30563ad1a56029a8829059a638055db
Hirofumi Shima <shima@peace2.jp>
2013-07-31 18:03:19 +0900

    css変更

---
+++ SP.Labo.Web/Controllers/R3OAuthController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SP.Labo.Web.Controllers
+{
+    public class R3OAuthController : Controller
+    {
+        //
+        // GET: /R3OAuth/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
