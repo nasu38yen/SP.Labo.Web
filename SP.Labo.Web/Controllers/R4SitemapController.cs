a2e8da6538f42cc7332963eec8452b784ffe64ca
Hirofumi Shima <shima@peace2.jp>
2013-08-07 10:10:00 +0900

    サイトマップを追加

---
+++ SP.Labo.Web/Controllers/R4SitemapController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SP.Labo.Web.Controllers
+{
+    public class R4SitemapController : Controller
+    {
+        //
+        // GET: /R4Sitemap/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
