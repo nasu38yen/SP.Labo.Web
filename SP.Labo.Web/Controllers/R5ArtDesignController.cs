d510c80993f2e3bbbb09a8a1d8d8bb37a9d7ae0f
Hirofumi Shima <shima@peace2.jp>
2013-08-08 08:36:55 +0900

    DnDを追加

---
+++ SP.Labo.Web/Controllers/R5ArtDesignController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SP.Labo.Web.Controllers
+{
+    public class R5ArtDesignController : Controller
+    {
+        //
+        // GET: /R5ArtDesign/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /R5ArtDesign/DnD
+
+        public ActionResult DnD()
+        {
+            return View();
+        }
+    }
+}
